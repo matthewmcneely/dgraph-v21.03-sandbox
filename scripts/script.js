ec13327b9683c97e4afe96f682203b210c00fcde
mattthew <matthew.mcneely@gmail.com>
2022-08-04 17:41:39 -0400

    Add support files

---
+++ scripts/script.js
@@ -0,0 +1,50 @@
+async function newUserWithScores({args, graphql}) {
+    console.log("ARGS", args)
+    let highScore = {
+        record_date: new Date(),
+        score: Math.max(...args.scores)
+    }
+    let scores = new Array()
+    args.scores.forEach(score => {
+        scores.push({
+            score: score,
+            record_date: new Date()
+        })
+    })    
+    const results = await graphql(`mutation ($username: String!, $scores: [GameScoreRef]!, $highScore: GameScoreRef!) {
+        addUser(input: [{username: $username, scores: $scores}]) {
+            user {
+                id
+            }
+        }
+    }`, {
+        "username": args.username, 
+        "scores": scores,
+    })
+    return results.data.addUser.user[0].id
+}
+
+
+async function calculateHighScore({parent, graphql}) {
+    const results = await graphql(`
+    query {
+        getUser(id: "${parent.id}") {
+          id
+          scores(order: {desc: score}, first: 1) {
+            score
+            record_date
+          }
+        }
+    }`)
+    return {
+        user: {id: parent.id},
+        score: results.data.getUser.scores[0].score,
+        record_date: results.data.getUser.scores[0].record_date
+    }
+}
+
+self.addGraphQLResolvers({
+    "User.high_score": calculateHighScore
+})
+
+
