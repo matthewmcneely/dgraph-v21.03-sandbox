5c351fe823da7bf9113f1f1b8b76ee635183fbbd
mattthew <matthew.mcneely@gmail.com>
2022-09-12 16:17:24 -0400

    Add example files

--- scripts/script.js
+++ scripts/script.js
@@ -1,1 +1,17 @@
-// Lambda code goes here
+async function newAuthor({args, graphql}) {
+    // lets give every new author a reputation of 3 by default
+    const results = await graphql(`mutation ($name: String!) {
+        addAuthor(input: [{name: $name, reputation: 3.0 }]) {
+            author {
+                id
+                reputation
+            }
+        }
+    }`, {"name": args.name})
+    console.log('results--------------\n', results)
+    return results.data.addAuthor.author[0].id
+}
+
+self.addGraphQLResolvers({
+    "Mutation.newAuthor": newAuthor
+})
