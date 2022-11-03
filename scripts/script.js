63b3777d5fc0464f26ee12a3546045b2fe1abd12
mattthew <matthew.mcneely@gmail.com>
2022-11-03 16:23:49 -0400

    Add more lambda types

--- scripts/script.js
+++ scripts/script.js
@@ -12,6 +12,21 @@ async function newAuthor({args, graphql}) {
     return results.data.addAuthor.author[0].id
 }
 
+async function authorsByName({args, dql}) {
+    const results = await dql.query(`query queryAuthor($name: string) {
+        queryAuthor(func: type(Author)) @filter(eq(Author.name, $name)) {
+            name: Author.name
+            reputation: Author.reputation
+        }
+    }`, {"$name": args.name})
+    console.log('results--------------\n', results)
+    return results.data.queryAuthor
+}
+
+const authorBio = ({parent: {name, reputation}}) => `My name is ${name} and my reputation is ${reputation}.`
+
 self.addGraphQLResolvers({
-    "Mutation.newAuthor": newAuthor
+    "Author.bio": authorBio,
+    "Mutation.newAuthor": newAuthor,
+    "Query.authorsByName": authorsByName,
 })
